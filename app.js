c6dbf3d1465f7e68b8b3119c29d16a24953fdb38
Kamie Short <kamie.short@gmail.com>
2022-01-25 13:00:14 -0800

    added let state and 1 event listener

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 // import functions and grab DOM elements
-const balleggshell = document.getElementbyID ('balleggshell')
-const ballseashell = document.getElementbyID ('ballseashell')
-const ballsnailshell = document.getElementbyID ('ballsnailshell')
+const eggshellball = document.getElementbyID ('eggshellball')
+const seashellball = document.getElementbyID ('seashellball')
+const snailshellball = document.getElementbyID ('snailshellball')
+
+const eggshellbutton = document.getElementById ('eggshellbutton')
+const eggshellbutton = document.getElementById ('seashellbutton')
+const eggshellbutton = document.getElementById ('snailshellbutton')
+
+const winSpan = document.getElementById('wins');
+const lossSpan = document.getElementById('losses');
+const totalSpan = document.getElementById('total');
 
 
 // let state
+let wins = 0;
+let total = 0
 
 // set event listeners 
+//reset reveal
+eggshellbutton.addEventListener (click, () => {
+  eggshellball.classList.remove('reveal')
+  seashellball.classList.remove('reveal')
+  snailshellball.classList.remove('reveal')
+
+total++
+
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
